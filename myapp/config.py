c74504b0f030c3829c02127d5f267a70826324f4
fumiya-kubota <fumiya.kubota@glucose.jp>
2018-12-13 04:38:57 +0900

    fix config

--- myapp/config.py
+++ myapp/config.py
@@ -2,14 +2,13 @@ import os
 
 
 class Config:
-    # DATABASE_HOST = 'postgresql+asyncpg://sapl@127.0.0.1:5432'
     DB_HOST = 'myapp@127.0.0.1:5432'
     DB_NAME = 'myapp'
 
 
 class TestingConfig:
     DB_HOST = 'myapp@127.0.0.1:5432'
-    DB_NAME = 'myapp'
+    DB_NAME = 'myapp-testing'
 
 
 def get_configuration():
