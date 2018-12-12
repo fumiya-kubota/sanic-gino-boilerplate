aa343e4c3af22cc2b07b9ab3623a317367d9c600
fumiya-kubota <fumiya.kubota@glucose.jp>
2018-12-13 03:30:31 +0900

    add comment

--- tests/http/test_todo.py
+++ tests/http/test_todo.py
@@ -7,11 +7,14 @@ async def test_todo_api(app, test_cli):
     """
     testing todo api
     """
+
+    # GET
     resp = await test_cli.get('/api/v1/todo')
     assert resp.status == 200
     resp_json = await resp.json()
     assert len(resp_json['todo_list']) == 0
 
+    # POST
     resp = await test_cli.post(
         '/api/v1/todo',
         data=json.dumps({
@@ -21,17 +24,20 @@ async def test_todo_api(app, test_cli):
     )
     assert resp.status == 201
 
+    # GET
     resp = await test_cli.get('/api/v1/todo')
     assert resp.status == 200
     resp_json = await resp.json()
     assert len(resp_json['todo_list']) == 1
     assert resp_json['todo_list'][0]['name'] == 'new_todo'
 
+    # DELETE
     resp = await test_cli.delete(
         '/api/v1/todo/1',
     )
     assert resp.status == 200
 
+    # GET
     resp = await test_cli.get('/api/v1/todo')
     assert resp.status == 200
     resp_json = await resp.json()
